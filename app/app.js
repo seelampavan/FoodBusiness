d1c740636eb88ebb52868357a90331c6da8d810b
meghanaG <meghanareddygudupally@gmail.com>
2024-03-03 18:35:03 +0000

    header and dashboard heading design

--- app/app.js
+++ app/app.js
@@ -6,10 +6,20 @@ var app = express();
 
 // Add static files location
 app.use(express.static("static"));
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+app.get('/dashboard', function (req, res) {
+    res.render('dashboard');
+});
 
 // Start server on port 3000
 app.listen(3000,function(){
---
+++ static/js/header.js
@@ -0,0 +1,10 @@
+$("nav .hamburger").click(function() {
+    $("ul").slideToggle();
+    
+
+     });
+    $(window).resize(function(){
+      if($(window).width() > 768){
+           $("ul").removeAttr('style');
+      }
+    });
