bbe6fc869a77917976a51a4314606eabcc0faba0
Haricharan <haricharananumalla@gmail.com>
2024-03-16 12:20:25 +0000

    Menu api and menu items ui

--- app/app.js
+++ app/app.js
@@ -14,12 +14,55 @@ app.set('views', './app/views');
 const db = require('./services/db');
 
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
-app.get('/dashboard', function (req, res) {
+// app.get('/', function (req, res) {
+//     res.send("hello world");
+// });
+app.get('/home', function (req, res) {
     res.render('dashboard');
 });
+
+// app.get('/', async (req, res) => {
+//     try {
+//         const items = await db.getAllItems();
+//         res.json(items);
+//         res.render('dashboard',{"items":items})
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+
+
+app.get("/", function (req, res) {
+    const sql = 'SELECT * FROM menu_item where category = "today_special" LIMIT 5';
+    const sql1 = 'SELECT * FROM menu_item WHERE veg = "Yes" LIMIT 5';
+    const sql2 = 'SELECT * FROM menu_item WHERE veg = "No" LIMIT 5';
+
+    Promise.all([db.query(sql), db.query(sql1),db.query(sql2)])
+        .then(([result, result1,result2]) => {
+            res.render('dashboard', { items: result , items1: result1 , items2: result2 });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        });
+});
+
+// Get item by ID
+app.get('/api/items/:id', async (req, res) => {
+    try {
+        const itemId = req.params.id;
+        const item = await db.getItemById(itemId);
+        if (!item) {
+            return res.status(404).json({ error: 'Item not found' });
+        }
+        res.json(item);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 
 // Start server on port 3000
 app.listen(3000,function(){
